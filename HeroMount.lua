f6279739780cb34e357272e1139b273ddfec4be8
Raphaël Bonnet <harcyah@gmail.com>
2019-09-20 21:32:34 +0200

    Add special "scenario" case

--- HeroMount.lua
+++ HeroMount.lua
@@ -13,38 +13,44 @@ local forceGroundMountZones = {
 }
 
 SlashCmdList['HERO_MOUNT'] = function()
+
 	if (IsMounted()) then
 		Dismount()
 		return
 	end
-	
+
 	if (UnitLevel("player") < 20) then
 		CastSpellByName("Invocation d’un chauffeur")
 		return
 	end
-	
+
 	if (UnitLevel("player") < 60) then
 		CastSpellByName("Seigneur corbeau")
 		return
 	end
-	
-	zone = GetRealZoneText()
+
+	local name, instanceType = GetInstanceInfo()
+	if (instanceType == "scenario") then
+		CastSpellByName("Seigneur corbeau")
+	end
+
+	local zone = GetRealZoneText()
 	if (has_value(forceGroundMountZones, zone)) then
 		CastSpellByName("Seigneur corbeau")
 	end
-	
-	timer, initial, maxvalue, scale, paused, label = GetMirrorTimerInfo(2)	
+
+	local timer, initial, maxvalue, scale, paused, label = GetMirrorTimerInfo(2)
 	if (timer == 'BREATH' and paused == 0) then
 		-- breath timer is running: player is underwater
 		CastSpellByName("Raie de la Grande mer")
 		return
 	end
-	
+
 	if (IsFlyableArea()) then
 		CastSpellByName("Expérience 12-B")
 		return
 	end
-	
+
 	CastSpellByName("Seigneur corbeau")
 end
 
